d21fbbf30f4416d72feb70960b846712fac269d6
Valentinas Kasteckis <41127259+Tortonas@users.noreply.github.com>
2021-01-30 18:16:38 +0200

    Added php scraper, started working on the scraping

--- src/Command/SyncCommand.php
+++ src/Command/SyncCommand.php
@@ -2,41 +2,100 @@
 
 namespace App\Command;
 
+use App\Entity\Ad;
+use DateTime;
+use DateTimeImmutable;
+use Doctrine\ORM\EntityManager;
+use Doctrine\ORM\EntityManagerInterface;
+use Goutte\Client;
 use Symfony\Component\Console\Command\Command;
-use Symfony\Component\Console\Input\InputArgument;
 use Symfony\Component\Console\Input\InputInterface;
-use Symfony\Component\Console\Input\InputOption;
 use Symfony\Component\Console\Output\OutputInterface;
 use Symfony\Component\Console\Style\SymfonyStyle;
+use Symfony\Component\HttpClient\HttpClient;
 
 class SyncCommand extends Command
 {
     protected static $defaultName = 'app:sync';
 
+    private EntityManagerInterface $entityManager;
+
+    /**
+     * SyncCommand constructor.
+     * @param EntityManagerInterface $entityManager
+     */
+    public function __construct(EntityManagerInterface $entityManager)
+    {
+        $this->entityManager = $entityManager;
+        parent::__construct();
+    }
+
     protected function configure()
     {
         $this
-            ->setDescription('Add a short description for your command')
-            ->addArgument('arg1', InputArgument::OPTIONAL, 'Argument description')
-            ->addOption('option1', null, InputOption::VALUE_NONE, 'Option description')
+            ->setDescription('Sync ads with database')
         ;
     }
 
     protected function execute(InputInterface $input, OutputInterface $output): int
     {
         $io = new SymfonyStyle($input, $output);
-        $arg1 = $input->getArgument('arg1');
 
-        if ($arg1) {
-            $io->note(sprintf('You passed an argument: %s', $arg1));
-        }
+        $client = new Client(HttpClient::create(['timeout' => 60]));
+        $crawler = $client->request('GET', 'https://sarg.lt/index.php?/cp/5-skelbimai/');
 
-        if ($input->getOption('option1')) {
-            // ...
-        }
 
-        $io->success('You have a new command! Now make it your own! Pass --help to see your options.');
+        $crawler->filter('article > div')->each(function ($node) {
+            $ads = explode('<br>', $node->html());
+            foreach ($ads as $ad) {
+                $this->generateAd($ad);
+            }
+        });
+
+        $io->success('Ads were synced!');
 
         return Command::SUCCESS;
+    }
+
+    private function generateAd(string $adText): void
+    {
+        $adText = $this->removeHtmlElements($adText);
+
+        $date = $this->getDate($adText);
+
+        $a = null;
+    }
+
+    private function removeHtmlElements(string $adText): string
+    {
+        $htmlElementsToRemove = [
+            '<b>',
+            '</b>'
+        ];
+
+        foreach ($htmlElementsToRemove as $htmlElement) {
+            $adText = str_replace($htmlElement, '', $adText);
+        }
+
+        return $adText;
+    }
+
+    private function getDate(string $adText): \DateTime
+    {
+        $dateStart = strpos($adText, '[');
+        $dateEnd = strpos($adText, ']');
+
+        $dateString = null;
+
+        for ($i = $dateStart+1; $i < $dateEnd; $i++) {
+            $dateString = $dateString . $adText[$i];
+        }
+
+        $a = new DateTimeImmutable();
+        $b = new DateTimeImmutable('2020-01-01');
+
+        var_dump($b);
+
+        return new DateTime();
     }
 }
