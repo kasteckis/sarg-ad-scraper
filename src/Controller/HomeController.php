51e965de2a02eb627a10a20f85de0235efe92019
Valentinas Kasteckis <valentinas.kasteckis@nfq.lt>
2021-01-30 23:00:58 +0200

    Added home page with bootstrap table that has simple search

--- src/Controller/HomeController.php
+++ src/Controller/HomeController.php
@@ -2,6 +2,7 @@
 
 namespace App\Controller;
 
+use App\Entity\Ad;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
@@ -13,8 +14,10 @@ class HomeController extends AbstractController
      */
     public function index(): Response
     {
-        return $this->render('home/index.html.twig', [
+        $ads = $this->getDoctrine()->getRepository(Ad::class)->findBy([], ['datetime' => 'desc']);
 
+        return $this->render('home/index.html.twig', [
+            'ads' => $ads
         ]);
     }
 }
